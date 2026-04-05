0df72e286d48aa8519d6955470005be42736f741
tushar <gsthushar44@gmail.com>
2026-04-06 01:02:11 +0530

    another change

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://recovery-tracker-backend-production.up.railway.app',
+  baseURL: 'https://recovery-tracker-backend-production.up.railway.app' ,
   headers: { 'Content-Type': 'application/json' },
 })
 
