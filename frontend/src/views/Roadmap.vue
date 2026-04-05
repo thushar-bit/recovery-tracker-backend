e78feb68ebb39f332a069b0f79d98cdd634b32e9
tushar <gsthushar44@gmail.com>
2026-04-06 00:37:58 +0530

    another minor change

--- frontend/src/views/Roadmap.vue
+++ frontend/src/views/Roadmap.vue
@@ -65,7 +65,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, onMounted } from 'vue'
+import { ref, computed, onMounted  } from 'vue'
 import { PHASES } from '../data'
 import { fetchRoadmap, toggleRoadmapTopic, fetchRoadmapStats } from '../api'
 
